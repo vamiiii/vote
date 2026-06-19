f5f552b2307cbc090235ae048508514730cc5c09
vamiiii <157354174+vamiiii@users.noreply.github.com>
2026-06-19 10:24:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ======================= API КЛИЕНТ =======================
-const API_BASE_URL = 'https://cc4ee635867bba.lhr.life';
+const API_BASE_URL = 'https://a1d650e4afb124.lhr.life';
 
 const api = {
     async get(endpoint) {
